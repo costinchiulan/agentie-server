2e606834b9b92971bddb7805fbb93f3096b8c541
Costi <costin.chiulan92@gmail.com>
2016-03-06 23:09:17 +0200

    Cleaning

--- app.js
+++ app.js
@@ -1,42 +1,21 @@
 var express = require('express'),
-    path = require('path'),
-    favicon = require('serve-favicon'),
     logger = require('morgan'),
-    ejs = require('ejs'),
-    cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose');
 
-require('./models/Client');
 require('./models/Bon');
-require('./models/Facturi');
-require('./models/User');
-mongoose.connect('mongodb://ds047955.mongolab.com:47955/facturi-ssw', {
-  user: 'facturier',
-  pass: '(&s^aDdD*&S'
+
+mongoose.connect('mongodb://localhost:27017/realEstate', {
+  //user: 'facturier',
+  //pass: '(&s^aDdD*&S'
 });
 
-var routes = require('./routes/index');
 var bonRoute = require('./routes/bonuri');
-var client = require('./routes/clients');
-var facturi = require('./routes/facturi');
-var generator = require('./routes/generator');
-var users = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
-app.engine('html', ejs.renderFile);
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -44,20 +23,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use('/', routes);
-app.use('/clients', client);
 app.use('/bonuri', bonRoute);
-app.use('/generator', generator);
-app.use('/facturi', facturi);
-app.use('/users', users);
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
 
 // development error handler
 // will print stacktrace
